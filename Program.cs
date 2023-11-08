83c360d6266f09346a4bbf7182bde842f09c515a
gamed <gamed@HUBERT-PC>
2023-11-08 17:02:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using ConsoleApp10;
+
+int intA = 5;
+int intB = intA;
+intA = 7;
+
+Console.WriteLine(intA);
+Console.WriteLine(intB);
+
+string stringA = "Adam";
+string stringB = stringA;
+stringA = "Paweł";
+
+Console.WriteLine(stringA);
+Console.WriteLine(stringB);
+
+bool boolA = true;
+bool boolB = boolA;
+boolA = false;
+
+Console.WriteLine(boolA);
+Console.WriteLine(boolB);
+
+Test testA = new Test();
+testA.Name = "Filip";
+testA.Age = 10;
+testA.IsTrueOrFalse = true;
+Test testB = testA;
+testA.Name = "Oskar";
+testA.Age = 24;
+testA.IsTrueOrFalse = false;
+
+Console.WriteLine($"Test A {testA.Name} {testA.Age} {testA.IsTrueOrFalse}");
+Console.WriteLine($"Test B {testB.Name} {testB.Age} {testB.IsTrueOrFalse}");
+
+Console.WriteLine("testA method:\n");
+testA.TestMethod(testA.Age);
+Console.WriteLine("testB method:\n");
+testB.TestMethod(testB.Age);
---
+++ Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Test
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool IsTrueOrFalse { get; set; }
+
+        public void TestMethod(int age)
+        {
+            age = Age;
+
+            for(int i = 0; i <= age; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
